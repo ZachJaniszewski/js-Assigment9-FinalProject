72dadad1894ad1e1399a52f1dd6a42e556b003da
Zachary Janiszewski <300088297@fvtc.edu>
2024-05-13 22:54:19 -0500

    site.js additions

--- public/site.js
+++ public/site.js
@@ -7,9 +7,36 @@
     const imageInput = document.querySelector('#image-input')
     const menuUl = document.querySelector('#menu-list')
 
+    // Update Menu Item 
+    const updateMenuItemForm = document.querySelector('#updateMenuItem')
+    const itemIdInput = document.querySelector('#itemId')
+    const newItemNameInput = document.querySelector('#newItemName')
+    const newItemPriceInput = document.querySelector('#newItemPrice')
+    const newItemDescriptionInput = document.querySelector('#newItemDescription')
 
-    // event 
+    // Delete Menu Item 
+    const deleteMenuItemForm = document.querySelector('#deleteMenuItem')
+    const deleteItemIdInput = document.querySelector('#deleteItemId')
+
+    //Event 
     const eventUl = document.querySelector('#event-list')
+
+    // Add Event 
+    const addEventForm = document.querySelector('#addEvent')
+    const eventNameInput = document.querySelector('#eventName')
+    const eventDateInput = document.querySelector('#eventDate')
+    const eventLocationInput = document.querySelector('#eventLocation')
+
+    // Update Event 
+    const updateEventForm = document.querySelector('#updateEvent')
+    const eventIdInput = document.querySelector('#eventId')
+    const newEventNameInput = document.querySelector('#newEventName')
+    const newEventDateInput = document.querySelector('#newEventDate')
+    const newEventLocationInput = document.querySelector('#newEventLocation')
+
+    // Delete Event 
+    const deleteEventForm = document.querySelector('#deleteEvent')
+    const deleteEventIdInput = document.querySelector('#deleteEventId')
     
 
     const getMenu = async () => {
@@ -75,6 +102,39 @@
     })
 
 
+    updateMenuItemForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        const itemId = itemIdInput.value
+        const newItemName = newItemNameInput.value
+        const newItemPrice = newItemPriceInput.value
+        const newItemDescription = newItemDescriptionInput.value
+    
+        await fetch(`/api/menu/${itemId}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name: newItemName, price: newItemPrice, description: newItemDescription })
+        })
+    
+        itemIdInput.value = ''
+        newItemNameInput.value = ''
+        newItemPriceInput.value = ''
+        newItemDescriptionInput.value = ''
+    
+        displayMenu(await getMenu())
+      })
+
+      deleteMenuItemForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        const itemId = deleteItemIdInput.value
+    
+        await fetch(`/api/menu/${itemId}`, {
+          method: 'DELETE'
+        })
+    
+        deleteItemIdInput.value = ''
+    
+        displayMenu(await getMenu())
+      })
     
 
     const displayEvents = events => {
@@ -105,9 +165,41 @@
 		
 	}
 
+
+  updateEventForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const eventId = eventIdInput.value
+    const newEventName = newEventNameInput.value
+    const newEventDate = newEventDateInput.value
+    const newEventLocation = newEventLocationInput.value
+
+    await fetch(`/api/events/${eventId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: newEventName, date: newEventDate, location: newEventLocation })
+    })
+
+    eventIdInput.value = ''
+    newEventNameInput.value = ''
+    newEventDateInput.value = ''
+    newEventLocationInput.value = ''
+
     displayEvents(await getEvents())
+  })
 
-    
+  deleteEventForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const eventId = deleteEventIdInput.value
 
+    await fetch(`/api/events/${eventId}`, {
+      method: 'DELETE'
+    })
 
+    deleteEventIdInput.value = ''
+
+    displayEvents(await getEvents())
+  })
+
+  displayMenu(await getMenu())
+  displayEvents(await getEvents())
 })()
